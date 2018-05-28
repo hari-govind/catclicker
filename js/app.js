cf54e3dce330326065ecbb0e5aee483bbba6efed
Hari Govind <hgs@disroot.org>
2018-05-28 21:43:49 +0530

    Added admin area

--- js/app.js
+++ js/app.js
@@ -3,8 +3,9 @@
     //model
 
    var model = {
+        adminIsHidden: true,
         currentCat: null,
-        cats : [
+        cats: [
         {'name':'brownie',
         'clicks':0,
         'url':'images/brownie.jpg'
@@ -34,6 +35,7 @@
             model.currentCat = model.cats[0];
             name_view.init();
             cat_view.init();
+            admin_view.init();
         },
         getCurrentCat: function(){
             return model.currentCat;
@@ -43,11 +45,30 @@
         },
         setCurrentCat: function(cat){
             model.currentCat = cat;
+            cat_view.render();
+            admin_view.render();
         },
         incrementCounter: function(){
             model.currentCat.clicks++;
             cat_view.render();
+            admin_view.render();
         },
+        setAdminIsHidden: function(stats){
+            model.adminIsHidden = stats;
+            admin_view.render();
+        },
+        getAdminIsHidden: function(){
+            return model.adminIsHidden;
+        },
+        updateCat: function(name, url,clicks){
+            cat = model.currentCat;
+            cat.name = name;
+            cat.url = url;
+            cat.clicks = clicks;
+            admin_view.render();
+            cat_view.render();
+            name_view.render();
+        }
     };
 
     //views
@@ -75,6 +96,7 @@
     var name_view = {
     init: function(){
         this.catList = document.getElementById('list');
+        this.adminElem = document.getElementById('admin-button');
         this.render();
     },
     render: function(){
@@ -91,14 +113,45 @@
             elem.addEventListener('click',(function(catCopy){
                 return function(){
                     octopus.setCurrentCat(catCopy);
-                    cat_view.render();
                 };
             })(cat));
             this.catList.appendChild(elem);
         }
+        this.adminElem.addEventListener('click', function(){
+            octopus.setAdminIsHidden(false);
+            cat_view.render();
+        });
     }
     };
-   
+   var admin_view = {
+    init: function(){
+        areaElem = document.getElementById('admin-area');
+        nameElem = document.getElementById('admin-name');
+        urlElem = document.getElementById('admin-url');
+        clicksElem = document.getElementById('admin-clicks');
+        cancelElem = document.getElementById('cancel-button');
+        updateElem = document.getElementById('update-button');
+        this.render();
+    },
+    render: function(){
+        if (!octopus.getAdminIsHidden()){
+            areaElem.style.visibility = 'visible';
+            currentCat = octopus.getCurrentCat();
+            nameElem.value = currentCat.name;
+            urlElem.value = currentCat.url;
+            clicksElem.value = currentCat.clicks;
+            updateElem.addEventListener('click', function(){
+                octopus.updateCat(nameElem.value,urlElem.value,clicksElem.value);
+            });
+        }
+        else{
+            areaElem.style.visibility = 'hidden';
+        }
+        cancelElem.addEventListener('click', function(){
+            octopus.setAdminIsHidden(true);
+        });
+    },
+   };
    
    
    octopus.init();
