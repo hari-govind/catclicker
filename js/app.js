45b6e1871933145b4855e6c0c318388b58ed52d2
Hari Govind <hgs@disroot.org>
2018-05-28 17:41:33 +0530

    changed catName to name

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
-$(function(){
+
 
     //model
 
    var model = {
         currentCat: null,
         cats : [
-        {'catName':'brownie',
+        {'name':'brownie',
         'clicks':0,
         'url':'images/brownie.jpg'
         },
-        {'catName':'blackie',
+        {'name':'blackie',
         'clicks':0,
         'url':'images/blackie.jpg'
         },
-        {'catName':'ed',
+        {'name':'ed',
         'clicks':0,
         'url':'images/ed.jpg'
         },
-        {'catName':'jim',
+        {'name':'jim',
         'clicks':0,
         'url':'images/jim.jpg'
         },
-        {'catName':'ivy',
+        {'name':'ivy',
         'clicks':0,
         'url':'images/ivy.jpg'
         }]
@@ -53,13 +53,11 @@ $(function(){
     //views
     
     
-    
     var cat_view = {
         init: function(){
                 this.catElem = document.getElementById('picture');
                 this.counterElem = document.getElementById('clicks');
                 this.nameElem = document.getElementById('name');
-                
                 this.catElem.addEventListener('click', function(){
                     octopus.incrementCounter();
                 });
@@ -69,7 +67,7 @@ $(function(){
         render: function(){
             var currentCat = octopus.getCurrentCat();
             this.counterElem.textContent = currentCat.clicks;
-            this.nameElem.textContent = currentCat.catName;
+            this.nameElem.textContent = currentCat.name;
             this.catElem.src = currentCat.url;
         }
         };
@@ -87,7 +85,7 @@ $(function(){
         for(i=0;i<cats.length;i++){
             cat = cats[i];
             elem = document.createElement('div');
-            elem.textContent = cat.catName;
+            elem.textContent = cat.name;
             
             //EventListener
             elem.addEventListener('click',(function(catCopy){
@@ -105,44 +103,3 @@ $(function(){
    
    octopus.init();
    
-   
-   
-   
-   
-});
-
-
-   
-
-
-/**
-var cats = ['brownie','blackie','ed','jim','ivy'];
-        var current_click = 0;
-        var clicks = [0,0,0,0,0];
-        var picture = document.getElementById('picture');
-        for(var i=0;i<cats.length;i++){
-            var cat = cats[i];
-
-            var cat_element = document.createElement('div');
-
-            cat_element.textContent = cat;
-
-            cat_element.addEventListener('click', (function(catCopy, number) {
-                return function(){
-                    picture.src = "images/"+catCopy+".jpg";
-                    current_click = number;
-                    document.getElementById('name').innerHTML = catCopy;
-                    clicks_element.innerHTML= clicks[current_click];
-                };
-            })(cat, i));
-
-            document.getElementById('list').appendChild(cat_element);
-
-        }
-        var clicks_element = document.getElementById('clicks')
-        var picture_element = document.getElementById('picture');
-        picture_element.addEventListener('click', function(){
-            clicks[current_click] += 1;
-            clicks_element.innerHTML= clicks[current_click];
-        });
-**/
